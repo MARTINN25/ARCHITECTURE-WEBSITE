02dc4147f8e872191ec7fcb4ebf790d6ecf74bf8
Maksym <gventon2504@gmail.com>
2023-02-16 20:28:09 +0000

    ARCHITECTURE-WEBSITE

---
+++ js/app.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+
+	setTimeout(function() {
+
+		document.body.classList.add('loaded')
+
+
+		Draggable.create('.gallery', {
+			bounds: 'body',
+			inertia: true
+		})
+	})
+};
